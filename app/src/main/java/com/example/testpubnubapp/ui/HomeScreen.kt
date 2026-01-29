a6d3351e3463a6e91eb06450fb486f3085f9118d
AlexKorRnd <AlexKorRnd@yandex.ru>
2026-01-29 22:23:22 +0300

    Expose DirectMessageItem type

--- app/src/main/java/com/example/testpubnubapp/ui/HomeScreen.kt
+++ app/src/main/java/com/example/testpubnubapp/ui/HomeScreen.kt
@@ -36,7 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
-private data class DirectMessageItem(
+data class DirectMessageItem(
     val id: String,
     val name: String
 )
